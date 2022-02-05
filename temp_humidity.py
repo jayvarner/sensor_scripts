9dbc243849cbaa81a506ea34ba7f8135b98ca641
Jay Varner <jayvarner@gmail.com>
2022-02-05 12:00:05 -0500

    Add MQTT

--- temp_humidity.py
+++ temp_humidity.py
@@ -8,12 +8,44 @@ Basic `AHTx0` example test
 import time
 import board
 import adafruit_ahtx0
+import paho.mqtt.client as mqtt
+import json
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
-while True:
-    print("\nTemperature: %0.1f C" % (sensor.temperature * 9 / 5 + 32))
-    print("Humidity: %0.1f %%" % sensor.relative_humidity)
-    time.sleep(2)
+clientname = 'home'
+hostname = '192.168.1.150'
+port = 1883
+timeout = 60
+
+config_file = open('config.json')
+
+config = json.load(config_file)
+
+# callback for CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+# callback for received messages
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client(clientname)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(config['mqttUser'], config['mqttPassword'])
+
+now = datetime.now().strftime('%Y-%m-%d %H:%M')
+
+try:
+    client.connect(hostname, port, timeout)
+    client.loop_start()
+    client.publish(f'{config['location']}_temp', (sensor.temperature * 9 / 5 + 32))
+    client.publish(f'{config['location']}_humidity', sensor.relative_humidity)
+    logging.info(f'Updated {now}')
+except Exception as e:
+    logging.error(f'Fuck, shit failed at {now}')
+    logging.error(e)
